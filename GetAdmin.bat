055432a0af0f9a84ff074785a794e070f6941832
AmazingHutao <116961586+AmazingHutao@users.noreply.github.com>
2022-11-28 20:29:33 +0800

    Add files via upload

---
+++ GetAdmin.bat
@@ -0,0 +1,15 @@
+@Echo Off
+
+cd %systemroot%\system32
+call :IsAdmin
+rem do somethings here
+Exit
+
+:IsAdmin
+Reg.exe query "HKU\S-1-5-19\Environment"
+If Not %ERRORLEVEL% EQU 0 (
+ Cls & Echo You must have administrator rights to continue ... 
+ Pause & Exit
+)
+Cls
+goto:eof
